14a0dc81eb0ddeec8c6bb1ac7808e8cf40acc59c
blbacelar <blbacelar@gmail.com>
2020-02-13 15:28:47 -0800

    Created custom form-input and button

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -12,10 +12,10 @@ const Header = () => (
         </Link>
 
         <div className='options'>
-            <Link className='option' to='shop'>Shop</Link>
-            <Link className='option' to='contact'>Contact</Link>
+            <Link className='option' to='shop'>SHOP</Link>
+            <Link className='option' to='contact'>CONTACT</Link>
+            <Link className='option' to='signin'>SIGN IN</Link>
         </div>
-
     </div>
 )
 
